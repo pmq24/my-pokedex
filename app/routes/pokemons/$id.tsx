96fe25ef13caecc69d344967ce63771443e0a905
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-12-06 18:16:09 +0700

    Install DaisyUI

--- app/routes/pokemons/$id.tsx
+++ app/routes/pokemons/$id.tsx
@@ -1,3 +1,14 @@
 export default function PokemonDetails() {
-  return <h1>Pokemon details</h1>;
+  return (
+    <>
+      <header className="navbar">
+        <img src="/postco.svg" alt="PostCo's logo" />
+      </header>
+      <input
+        type="text"
+        placeholder="Find pokemon..."
+        className="input w-full max-w-xs"
+      />
+    </>
+  );
 }

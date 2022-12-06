f023c947a35e5c0c758feff4520c0e81a8984c96
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-12-06 21:21:28 +0700

    Add PokemonDetails page (unfinished)

--- app/routes/__app/pokemons/$id.tsx
+++ app/routes/__app/pokemons/$id.tsx
@@ -1,3 +1,43 @@
+import { type LoaderFunction, redirect } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { capitalize } from 'lodash';
+import Container from 'app/components/container';
+
+export const loader: LoaderFunction = async function (data) {
+  const id = data.params.id;
+
+  if (!id || isNaN(+id) || +id < 0) {
+    throw redirect('/');
+  }
+
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  return res.json();
+};
+
 export default function PokemonDetails() {
-  return <h1>Pokemon Details</h1>;
+  const pokemon = useLoaderData();
+  return (
+    <Container>
+      <div className="prose">
+        <h1>{capitalize(pokemon.name)}</h1>
+      </div>
+      <section className="flex">
+        <section className="w-1/3">
+          <img
+            className="w-full"
+            src={pokemon.sprites.front_default}
+            alt="Pokemon's front sprite"
+          />
+        </section>
+        <section className="w-2/3">
+          {pokemon.types.map((type: any) => (
+            <span className="badge mr-1" key={type.slot}>
+              {type.type.name}
+            </span>
+          ))}
+        </section>
+      </section>
+    </Container>
+  );
 }

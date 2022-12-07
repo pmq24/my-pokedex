37e8897cef11536f1d259ee1aba03abaf5bb2c07
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-12-07 14:19:33 +0700

    Design AutoComplete for SearchField

--- app/components/search-field.tsx
+++ app/components/search-field.tsx
@@ -1,6 +1,11 @@
+import { capitalize } from 'lodash';
+import { useState } from 'react';
+
 export default function SearchField() {
+  const [searchResult, setSearchResult] = useState([]);
+
   return (
-    <form className="w-full flex">
+    <div className="relative w-full flex">
       <input
         type="text"
         placeholder="Find Pokemon..."
@@ -9,6 +14,30 @@ export default function SearchField() {
       <button className="btn btn-square flex-none">
         <img src="/icons/search.svg" className="h-6 w-6" alt="Search icon" />
       </button>
-    </form>
+      <ul className="menu absolute top-full w-full bg-base-100 shadow-md rounded-box">
+        <EmptyAutoCompleteEntry />
+      </ul>
+    </div>
+  );
+}
+
+type SearchResult = {
+  name: string;
+  id: number;
+};
+
+function EmptyAutoCompleteEntry() {
+  return (
+    <li className="disabled">
+      <button className="btn-disabled">No result</button>
+    </li>
+  );
+}
+
+function AutoCompleteEntry({ name, id }: SearchResult) {
+  return (
+    <li>
+      <a href={`https://pokeapi.co/api/v2/pokemon/${id}`}>{capitalize(name)}</a>
+    </li>
   );
 }

c19f72f06730dfc2df04f583baf78a4a0b77451a
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-12-07 22:45:42 +0700

    Add owned pokemon in home page

--- app/routes/__app/index.tsx
+++ app/routes/__app/index.tsx
@@ -1,3 +1,4 @@
+import { capitalize } from 'lodash';
 import { useContext, useEffect, useState } from 'react';
 import Container from '../../components/container';
 import { PokemonStoreContext } from '../../lib/pokemon-store';
@@ -12,9 +13,32 @@ export default function Home() {
 
   return (
     <Container>
-      {pokemons.map((pokemon) => (
-        <span key={pokemon.name}>{pokemon.name}</span>
-      ))}
+      <div className="prose">
+        <h1>My Pokemons</h1>
+      </div>
+      <div className="flex">
+        {pokemons.map((pokemon) => (
+          <a
+            className="w-1/3"
+            key={pokemon.id}
+            href={`http://localhost:3000/pokemons/${pokemon.id}`}
+          >
+            <article className="shadow-md">
+              <figure>
+                <img
+                  src={
+                    pokemon.sprites.other['official-artwork']['front_default']
+                  }
+                  alt={`${pokemon.name}`}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{capitalize(pokemon.name)}</h2>
+              </div>
+            </article>
+          </a>
+        ))}
+      </div>
     </Container>
   );
 }

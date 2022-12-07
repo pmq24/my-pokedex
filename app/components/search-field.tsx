8b0c1eeedeb600b7e1a0272430c380e30c0bb7ca
Phạm Minh Quang <quangpham.245@protonmail.com>
2022-12-07 14:53:10 +0700

    Finish autocomplete

--- app/components/search-field.tsx
+++ app/components/search-field.tsx
@@ -1,8 +1,37 @@
 import { capitalize } from 'lodash';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { GraphQLClient } from 'graphql-request';
+
+const client = new GraphQLClient('https://beta.pokeapi.co/graphql/v1beta');
+
+async function search(query: string) {
+  return await client.request(
+    `
+      query samplePokeAPIquery($searchQuery: String!) {
+        pokemon_v2_pokemon(
+          limit: 10
+          where: { name: { _ilike: $searchQuery } }
+        ) {
+          id
+          name
+        }
+      }
+    `,
+    { searchQuery: `%${query}%` }
+  );
+}
 
 export default function SearchField() {
-  const [searchResult, setSearchResult] = useState([]);
+  const [searchString, setSearchString] = useState('');
+  const [searchResult, setSearchResult] = useState<SearchResult[]>([]);
+
+  const shouldHideAutoComplete = !searchString.trim();
+
+  useEffect(() => {
+    search(searchString).then((result) =>
+      setSearchResult(result.pokemon_v2_pokemon)
+    );
+  }, [searchString]);
 
   return (
     <div className="relative w-full flex">
@@ -10,12 +39,21 @@ export default function SearchField() {
         type="text"
         placeholder="Find Pokemon..."
         className="input input-bordered flex-1"
+        value={searchString}
+        onChange={(event) => setSearchString(event.target.value)}
       />
-      <button className="btn btn-square flex-none">
-        <img src="/icons/search.svg" className="h-6 w-6" alt="Search icon" />
-      </button>
-      <ul className="menu absolute top-full w-full bg-base-100 shadow-md rounded-box">
-        <EmptyAutoCompleteEntry />
+      <ul
+        className={` ${
+          shouldHideAutoComplete ? 'hidden' : ''
+        } menu absolute top-full w-full bg-base-100 shadow-md rounded-box z-50`}
+      >
+        {!searchResult || !searchResult.length ? (
+          <EmptyAutoCompleteEntry />
+        ) : (
+          searchResult.map((result) => (
+            <AutoCompleteEntry key={result.id} {...result} />
+          ))
+        )}
       </ul>
     </div>
   );
@@ -37,7 +75,7 @@ function EmptyAutoCompleteEntry() {
 function AutoCompleteEntry({ name, id }: SearchResult) {
   return (
     <li>
-      <a href={`https://pokeapi.co/api/v2/pokemon/${id}`}>{capitalize(name)}</a>
+      <a href={`http://localhost:3000/pokemons/${id}`}>{capitalize(name)}</a>
     </li>
   );
 }
